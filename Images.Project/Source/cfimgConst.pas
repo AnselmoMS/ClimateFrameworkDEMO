b4bb2eabae1df62f24e04e8a857568bcb7af90db
Anselmo Mota <anselmo.com@gmail.com>
2021-10-09 12:14:33 -0300

    Initial commit to Images Project

---
+++ Images.Project/Source/cfimgConst.pas
@@ -0,0 +1,17 @@
+unit cfimgConst;
+
+interface
+
+const
+  DLLIMAGE_VERSION = 20211009; //TcfFileVersion = (Major: 1; Minor: 0; Release: 0; Build: 0);
+
+type
+  TcfImages = (
+    cfCloud,
+    cfRain,
+    cfSun //...
+  );
+
+implementation
+
+end.
